ff68791ba972e488e04378764f1fe7a8935039ff
masaud04 <ahmedmasaud942@gmail.com>
2025-07-26 20:15:46 +0600

    fix: Update CORS origin to use production URL for enhanced security

--- Server/src/app.js
+++ Server/src/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://agencio-liard.vercel.app",
     credentials: true,
   })
 );
