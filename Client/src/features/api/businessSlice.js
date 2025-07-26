2878eaeeaa148ad03006377aaf47eceed2ef0d9a
masaud04 <ahmedmasaud942@gmail.com>
2025-07-26 19:57:24 +0600

    feat: Update API base URL to use environment variable for better configuration management

--- Client/src/features/api/businessSlice.js
+++ Client/src/features/api/businessSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:7000/api/v1";
+// const baseUrl = "http://localhost:7000/api/v1";
+const baseUrl = import.meta.env.VITE_API_URL
 
 export const businessApi = createApi({
   reducerPath: "businessApi",
--- Client/src/features/api/contentSlice.js
+++ Client/src/features/api/contentSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "http://localhost:7000/api/v1";
+const baseUrl = import.meta.env.VITE_API_URL;
 
 export const contentSlice = createApi({
   reducerPath: "contentApi",
